98c40c72e1decabcbf0f3ef228ec3605c4ec0aec
Donelle Reed <donellereed@gmail.com>
2019-06-27 02:53:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray (arr, element){
+  return [element, ...arr]
+}
+
+function destructivelyAddElementToBeginningOfArray(arr, element){
+  return arr.unshift(element)
+}
